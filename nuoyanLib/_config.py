e121c15482635f3ed68513aeacb461f2a904454e
Nuoyan <1279735247@qq.com>
2023-04-02 01:49:51 +0800

    v0.4.0: 补全服务端事件 事件回调函数名统一改为与事件名一致

--- nuoyanLib/_config.py
+++ nuoyanLib/_config.py
@@ -12,12 +12,12 @@
 #   Author        : Nuoyan
 #   Email         : 1279735247@qq.com
 #   Gitee         : https://gitee.com/charming-lee
-#   Last Modified : 2023-03-27
+#   Last Modified : 2023-04-02
 #
 # ====================================================
 
 
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 __authorname__ = "诺言Nuoyan"
 __authorqq__ = "1279735247"
 __authoremail__ = "1279735247@qq.com"
