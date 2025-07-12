377a72d2113d08041942c1a2bde84dc132113e5b
Nuoyan <1279735247@qq.com>
2025-07-13 04:41:40 +0800

    feat: 删掉了NuoyanLibClientSystem.register_and_create_ui

--- nuoyanlib/_core/_client/_lib_client.py
+++ nuoyanlib/_core/_client/_lib_client.py
@@ -92,38 +92,6 @@ class NuoyanLibClientSystem(ClientEventProxy, NuoyanLibBaseSystem, ClientSystem)
             'sys_name': sys_name,
         })
 
-    def register_and_create_ui(self, namespace, ui_key, cls_path, ui_screen_def, stack=False, param=None):
-        """
-        | 注册并创建UI。
-        | 如果UI已创建，则返回其实例。
-        | 使用该接口创建的UI，其UI类 ``.__init__()`` 方法的 ``param`` 参数会自带一个名为 ``__cs__`` 的key，对应的值为创建UI的客户端的实例，可以方便地调用客户端的属性、方法和接口。
-
-        -----
-
-        :param str namespace: 命名空间，建议为mod名字
-        :param str ui_key: UI唯一标识
-        :param str cls_path: UI类路径
-        :param str ui_screen_def: UI画布路径，格式为"namespace.scree_name"，namespace对应UI json文件中"namespace"的值，scree_name为想打开的画布的名称（一般为main）
-        :param bool stack: 是否使用堆栈管理的方式创建UI，默认为False
-        :param dict|None param: 创建UI的参数字典，会传到UI类__init__方法的param参数中，默认为空字典
-
-        :return: UI类实例，注册或创建失败时返回None
-        :rtype: _ScreenNode|None
-        """
-        node = client_api.GetUI(namespace, ui_key)
-        if node:
-            return node
-        if not client_api.RegisterUI(namespace, ui_key, cls_path, ui_screen_def):
-            return
-        if param is None:
-            param = {}
-        if isinstance(param, dict):
-            param['__cs__'] = self
-        if stack:
-            return client_api.PushScreen(namespace, ui_key, param)
-        else:
-            return client_api.CreateUI(namespace, ui_key, param)
-
     # set_query_mod_var ================================================================================================
 
     def _SetQueryCache(self, args):
