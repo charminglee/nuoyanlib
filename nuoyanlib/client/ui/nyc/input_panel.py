5024ed0e1142766c1427575de5561cbb3aea5115
Nuoyan <1279735247@qq.com>
2025-07-22 04:52:19 +0800

    feat: 完善NyInputPanel

--- nuoyanlib/client/ui/nyc/input_panel.py
+++ nuoyanlib/client/ui/nyc/input_panel.py
@@ -7,7 +7,7 @@
 |   Author: Nuoyan
 |   Email : 1279735247@qq.com
 |   Gitee : https://gitee.com/charming-lee
-|   Date  : 2025-06-11
+|   Date  : 2025-07-22
 |
 | ==============================================
 """
@@ -46,6 +46,76 @@ class NyInputPanel(NyControl):
     # endregion
 
     # region property proxy ============================================================================================
+
+    @property
+    def is_modal(self):
+        """
+        [可读写属性]
+
+        | 当前面板是否为模态框。
+
+        :rtype: bool
+        """
+        return self.base_control.GetIsModal()
+
+    @is_modal.setter
+    def is_modal(self, val):
+        """
+        [可读写属性]
+
+        | 当前面板是否为模态框。
+
+        :type val: bool
+        """
+        if isinstance(val, int):
+            val = bool(val)
+        self.base_control.SetIsModal(val)
+
+    @property
+    def is_swallow(self):
+        """
+        [可读写属性]
+
+        | 当前面板输入是否会吞噬事件，为 ``True`` 时，点击事件不会穿透到世界，如破坏方块、镜头转向不会被响应。
+
+        :rtype: bool
+        """
+        return self.base_control.GetIsSwallow()
+
+    @is_swallow.setter
+    def is_swallow(self, val):
+        """
+        [可读写属性]
+
+        | 当前面板输入是否会吞噬事件，为 ``True`` 时，点击事件不会穿透到世界，如破坏方块、镜头转向不会被响应。
+
+        :type val: bool
+        """
+        if isinstance(val, int):
+            val = bool(val)
+        self.base_control.SetIsSwallow(val)
+
+    @property
+    def offset_delta(self):
+        """
+        [可读写属性]
+
+        | 拖拽偏移量。
+
+        :rtype: tuple[float,float]
+        """
+        return self.base_control.GetOffsetDelta()
+
+    @offset_delta.setter
+    def offset_delta(self, val):
+        """
+        [可读写属性]
+
+        | 拖拽偏移量。
+
+        :type val: tuple[float,float]
+        """
+        self.base_control.SetOffsetDelta(tuple(val))
 
     # endregion
 
--- nuoyanlib/client/ui/nyc/input_panel.pyi
+++ nuoyanlib/client/ui/nyc/input_panel.pyi
@@ -18,6 +18,7 @@ from mod.client.ui.controls.inputPanelUIControl import InputPanelUIControl
 from .control import NyControl
 from ..screen_node import ScreenNodeExtension
 from ...._core._types._checker import args_type_check
+from ...._core._types._typing import FTuple2
 
 
 class NyInputPanel(NyControl):
@@ -33,4 +34,76 @@ class NyInputPanel(NyControl):
     @args_type_check(str, is_method=True)
     def __div__(self, other: str) -> Optional[NyControl]: ...
     def __truediv__(self, other: str) -> Optional[NyControl]: ... # for python3
+    @property
+    def is_modal(self) -> bool: ...
+    @is_modal.setter
+    def is_modal(self, val: bool) -> None: ...
+    @property
+    def is_swallow(self) -> bool: ...
+    @is_swallow.setter
+    def is_swallow(self, val: bool) -> None: ...
+    @property
+    def offset_delta(self) -> FTuple2: ...
+    @offset_delta.setter
+    def offset_delta(self, val: FTuple2) -> None: ...
 
+    def SetIsModal(self, is_modal) -> bool:
+        """
+        | 设置当前面板是否为模态框。
+
+        -----
+
+        :param bool is_modal: 是否为模态框
+
+        :return: 是否成功
+        :rtype: bool
+        """
+    def GetIsModal(self) -> bool:
+        """
+        | 判断当前面板是否为模态框。
+
+        -----
+
+        :return: 当前面板是否为模态框
+        :rtype: bool
+        """
+    def SetIsSwallow(self, is_swallow: bool) -> bool:
+        """
+        | 设置当前面板输入是否会吞噬事件， ``is_swallow`` 为 ``Ture`` 时，点击时，点击事件不会穿透到世界。如破坏方块、镜头转向不会被响应。
+
+        -----
+
+        :param bool is_swallow: 是否吞噬事件
+
+        :return: 是否成功
+        :rtype: bool
+        """
+    def GetIsSwallow(self) -> bool:
+        """
+        | 判断当前面板输入是否会吞噬事件， ``is_swallow`` 为 ``Ture`` 时，点击时，点击事件不会穿透到世界。如破坏方块、镜头转向不会被响应。
+
+        -----
+
+        :return: 当前面板输入是否会吞噬事件
+        :rtype: bool
+        """
+    def SetOffsetDelta(self, offset_delta: FTuple2) -> bool:
+        """
+        | 设置点击面板的拖拽偏移量。
+
+        -----
+
+        :param tuple[float,float] offset_delta: 拖拽偏移量
+
+        :return: 是否成功
+        :rtype: bool
+        """
+    def GetOffsetDelta(self) -> FTuple2:
+        """
+        | 获得点击面板的拖拽偏移量。
+
+        -----
+
+        :return: 拖拽偏移量
+        :rtype: tuple[float,float]
+        """
