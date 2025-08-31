5aa57eb593b7db2598526ec984546c710c675d5d
Nuoyan <1279735247@qq.com>
2025-08-31 18:12:21 +0800

    feat: 添加3.5 DrawingCompClient组件

--- src/nuoyanlib/_core/_client/comp.pyi
+++ src/nuoyanlib/_core/_client/comp.pyi
@@ -7,7 +7,7 @@
 |   Author: Nuoyan
 |   Email : 1279735247@qq.com
 |   Gitee : https://gitee.com/charming-lee
-|   Date  : 2025-06-05
+|   Date  : 2025-08-31
 |
 | ==============================================
 """
@@ -18,16 +18,16 @@ from mod.client.system.clientSystem import ClientSystem
 from mod.client.ui.screenNode import ScreenNode
 from mod.client.ui.viewBinder import ViewBinder
 from mod.client.ui.viewRequest import ViewRequest
+from mod.client.ui.CustomUIScreenProxy import CustomUIScreenProxy
+from mod.client.ui.CustomUIControlProxy import CustomUIControlProxy
+from mod.client.ui.NativeScreenManager import NativeScreenManager
 from mod.client.component.engineCompFactoryClient import EngineCompFactoryClient
-from mod.client.component.dimensionCompClient import DimensionCompClient
-from mod.client.component.neteaseWindowCompClient import NeteaseWindowCompClient
-from mod.client.component.achievementCompClient import AchievementCompClient
 from mod.client.component.skyRenderCompClient import SkyRenderCompClient
 from mod.client.component.frameAniTransComp import FrameAniTransComp
 from mod.client.component.actorRenderCompClient import ActorRenderCompClient
 from mod.client.component.actionCompClient import ActionCompClient
 from mod.client.component.itemCompClient import ItemCompClient
-from mod.client.component.blockGeometryCompClient import BlockGeometryCompClient
+from mod.client.component.frameAniControlComp import FrameAniControlComp
 from mod.client.component.attrCompClient import AttrCompClient
 from mod.client.component.textNotifyCompClient import TextNotifyComponet
 from mod.client.component.particleSkeletonBindComp import ParticleSkeletonBindComp
@@ -38,7 +38,7 @@ from mod.client.component.particleEntityBindComp import ParticleEntityBindComp
 from mod.client.component.virtualWorldCompClient import VirtualWorldCompClient
 from mod.client.component.cameraCompClient import CameraComponentClient
 from mod.client.component.engineTypeCompClient import EngineTypeComponentClient
-from mod.client.component.frameAniControlComp import FrameAniControlComp
+from mod.client.component.blockGeometryCompClient import BlockGeometryCompClient
 from mod.client.component.actorMotionCompClient import ActorMotionComponentClient
 from mod.client.component.frameAniSkeletonBindComp import FrameAniSkeletonBindComp
 from mod.client.component.healthCompClient import HealthComponentClient
@@ -57,10 +57,11 @@ from mod.client.component.modelCompClient import ModelComponentClient
 from mod.client.component.nameCompClient import NameComponentClient
 from mod.client.component.gameCompClient import GameComponentClient
 from mod.client.component.particleSystemCompClient import ParticleSystemCompClient
-from mod.client.component.particleTransComp import ParticleTransComp
+from mod.client.component.neteaseWindowCompClient import NeteaseWindowCompClient
 from mod.client.component.configCompClient import ConfigCompClient
 from mod.client.component.playerCompClient import PlayerCompClient
 from mod.client.component.fogCompClient import FogCompClient
+from mod.client.component.particleTransComp import ParticleTransComp
 from mod.client.component.textBoardCompClient import TextBoardComponentClient
 from mod.client.component.postProcessControlComp import PostProcessComponent
 from mod.client.component.auxValueCompClient import AuxValueComponentClient
@@ -70,14 +71,14 @@ from mod.client.component.frameAniEntityBindComp import FrameAniEntityBindComp
 from mod.client.component.operationCompClient import OperationCompClient
 from mod.client.component.chunkSourceCompClient import ChunkSourceCompClient
 from mod.client.component.rotCompClient import RotComponentClient
+from mod.client.component.drawingCompClient import DrawingCompClient
 from mod.client.component.collisionBoxCompClient import CollisionBoxComponentClient
 from mod.client.component.particleControlComp import ParticleControlComp
 from mod.client.component.tameCompClient import TameComponentClient
 from mod.client.component.modAttrCompClient import ModAttrComponentClient
 from mod.client.component.playerAnimCompClient import PlayerAnimCompClient
-from mod.client.ui.CustomUIScreenProxy import CustomUIScreenProxy
-from mod.client.ui.CustomUIControlProxy import CustomUIControlProxy
-from mod.client.ui.NativeScreenManager import NativeScreenManager
+from mod.client.component.achievementCompClient import AchievementCompClient
+from mod.client.component.dimensionCompClient import DimensionCompClient
 from .._types._typing import FTuple2
 from .._utils import CachedObject
 
@@ -257,6 +258,7 @@ class CF(CachedObject):
     _target: str
     def __init__(self: ..., target: str) -> None: ...
     def __getattr__(self, name: str) -> Any: ...
+    Drawing: DrawingCompClient
     Achievement: AchievementCompClient
     Action: ActionCompClient
     ActorMotion: ActorMotionComponentClient
