6c3cfffc90fb56b1c8956cbcbae00a02fd2be136
Nuoyan <1279735247@qq.com>
2026-09-13 03:24:52 +0800

    refactor(NyItemRenderer): 合并item_name和item_aux属性

--- src/nuoyanlib/client/ui/nyc/item_renderer.py
+++ src/nuoyanlib/client/ui/nyc/item_renderer.py
@@ -28,6 +28,19 @@ class NyItemRenderer(NyControl):
     """
     物品渲染器控件类。
 
+    示例
+    ----
+
+    >>> self.item_renderer = nyl.NyItemRenderer(self, "/panel/item_renderer")
+    >>> self.item_renderer.item = "minecraft:diamond", 0
+    >>> self.item_renderer.is_enchanted = True
+
+    参见
+    ----
+
+    - ``NyItemRenderer.item`` -- 获取/设置物品渲染器显示的物品的 identifier 和特殊值。
+    - ``NyItemRenderer.is_enchanted`` -- 获取/设置物品渲染器显示的物品是否附魔。
+
     -----
 
     :param NyScreenNode|NyScreenProxy ny_screen_node: 持有该物品渲染器控件的 NyScreenNode 或 NyScreenProxy 实例
@@ -45,48 +58,27 @@ class NyItemRenderer(NyControl):
     # region Properties ================================================================================================
 
     @property
-    def item_name(self):
+    def item(self):
         """
         [可读写属性]
 
-        ItemRenderer 显示的物品的 identifier。
+        物品渲染器显示的物品的 identifier 和特殊值。
 
-        :rtype: str
+        :rtype: tuple[str,int]
         """
-        return self._base_control.GetUiItem().get('itemName', "")
+        item = self._base_control.GetUiItem()
+        return item.get('itemName', ""), item.get('auxValue', 0)
 
-    @item_name.setter
-    def item_name(self, val):
-        """
-        [可读写属性]
-
-        ItemRenderer 显示的物品的 identifier。
-
-        :type val: str
-        """
-        self._base_control.SetUiItem(val, self.item_aux, self.is_enchanted)
-
-    @property
-    def item_aux(self):
+    @item.setter
+    def item(self, val):
         """
         [可读写属性]
 
-        ItemRenderer 显示的物品的特殊值。
+        物品渲染器显示的物品的 identifier 和特殊值。
 
-        :rtype: int
+        :type val: tuple[str,int]
         """
-        return self._base_control.GetUiItem().get('auxValue', 0)
-
-    @item_aux.setter
-    def item_aux(self, val):
-        """
-        [可读写属性]
-
-        ItemRenderer 显示的物品的特殊值。
-
-        :type val: int
-        """
-        self._base_control.SetUiItem(self.item_name, val, self.is_enchanted)
+        self._base_control.SetUiItem(val[0], val[1], self.is_enchanted)
 
     @property
     def is_enchanted(self):
@@ -108,7 +100,8 @@ class NyItemRenderer(NyControl):
 
         :type val: bool
         """
-        self._base_control.SetUiItem(self.item_name, self.item_aux, val)
+        item = self.item
+        self._base_control.SetUiItem(item[0], item[1], val)
 
     @property
     def user_data(self):
@@ -130,7 +123,8 @@ class NyItemRenderer(NyControl):
 
         :type val: dict|None
         """
-        self._base_control.SetUiItem(self.item_name, self.item_aux, self.is_enchanted, val)
+        item = self.item
+        self._base_control.SetUiItem(item[0], item[1], self.is_enchanted, val)
 
     # endregion
 
